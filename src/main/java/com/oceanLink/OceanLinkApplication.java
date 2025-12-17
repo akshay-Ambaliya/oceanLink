b0ec167431979d8780abb7cb41fddde8792764cf
Akshay <akshayambaliya8542@gmail.com>
2025-12-17 13:55:06 +0530

    Initial commit

---
+++ src/main/java/com/oceanLink/OceanLinkApplication.java
@@ -0,0 +1,13 @@
+package com.oceanLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oceanLink/OceanLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
